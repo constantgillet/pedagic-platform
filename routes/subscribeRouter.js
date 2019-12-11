2060526a9f7131820101d790fb8d10e4878f1d40
constantgillet <constant.gillet@hetic.net>
2019-12-11 12:36:58 +0100

    :bookmark: Updating subscribe back form control

--- routes/subscribeRouter.js
+++ routes/subscribeRouter.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-
+const sha1 = require('sha1')
 const mysqlConf = require('../config').mysql_pool;
 
 /* GET home page. */
@@ -27,18 +27,45 @@ router.post('/', (req, res) => {
           password = req.body.password,
           passwordConfirmation = req.body.passwordConfirmation
 
-    //Controlling length of the name and firstname
+    //Controling length of the name and firstname
     if(name.length < 25 && name.length != 0 && firstName.length < 25 && firstName.length != 0) {
-      res.render('subscribe', { alertMessage: 'email posté' })
+      
+      //Controling if i's an email
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) { 
 
-      
-      //Controlling email
-      
+        //Controling gender and usertype
+        if((gender == 'male' || gender == 'female') && (userType == 'teacher' || userType == 'student')) {
+
+          //Controlling password length
+          if(password.length > 5) {
+
+            //Hash passwords with Sha1
+            sha1(password)
+            sha1(passwordConfirmation)
+
+            //We control if password match
+            if(password === passwordConfirmation) {
+              res.render('subscribe', { alertMessage: 'Votre compte a été créé.'})
+            }
+            else {
+              res.render('subscribe', { alertMessage: 'Les mots de passe ne correspondent pas.'})
+            }
+          }
+          else {
+            res.render('subscribe', { alertMessage: 'Votre mot de passe est trop court.'})
+          }
+        }
+        else {
+          res.render('subscribe', { alertMessage: 'Erreur de sexe.'})
+        }
+      }
+      else {
+        res.render('subscribe', { alertMessage: 'Votre email est invalide.'})
+      }      
     }
     else {
-      res.render('subscribe', { alertMessage: 'Votre nom ou prénom est trop long ou trop court' })
-    }
-    
+      res.render('subscribe', { alertMessage: 'Votre nom ou prénom est trop long ou trop court.' })
+    } 
   }
   else {
     res.render('subscribe', { alertMessage: 'email trop court' })
