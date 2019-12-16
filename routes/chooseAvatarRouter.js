42199897661016f770951f13e5c3a3d2b9c5f3e3
constantgillet <constant.gillet@hetic.net>
2019-12-16 22:06:10 +0100

    :sparkles: Adding form controls in choose Avatar

--- routes/chooseAvatarRouter.js
+++ routes/chooseAvatarRouter.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const router = express.Router()
+//We connect to the database
+const db = require('../dbConnect')
 
 /* GET  page. */
 router.get('/', (req, res) => {
-    res.render('chooseAvatar')
+    if(typeof(req.session.loggedIn) != 'undefined') {
+        
+        if(req.session.loggedIn) {
+            res.render('chooseAvatar')
+            console.log(req.session.loggedIn)  
+        }
+        else {
+            console.log(req.session.loggedIn)
+            res.redirect('login')
+        }
+    }
+    else {
+        res.redirect('login')
+    }
+})
+
+//If the client completed the form
+router.post('/', (req, res) => {
+    if(typeof(req.body.gender) != 'undefined') {
+        if(req.body.gender == 'male' || req.body.gender == 'female') {
+            
+            const sql = `INSERT INTO avatars (gender) VALUES ('${ req.body.gender }')`
+            
+            //We put into the database
+        }
+    }
 })
 
 module.exports = router
