9ac1cbe61021669a6cb92677022a0dc5ab27b66d
aurelbnn <aurel.bonneau1507@gmail.com>
2019-12-20 01:55:57 +0100

    :sparkles: add new page for history chapters presentation

---
+++ public/scripts/chapters.js
@@ -0,0 +1,76 @@
+const more1 = document.querySelector('.js-more-1')
+const more2 = document.querySelector('.js-more-2')
+const more3 = document.querySelector('.js-more-3')
+const more4 = document.querySelector('.js-more-4')
+const more5 = document.querySelector('.js-more-5')
+const more6 = document.querySelector('.js-more-6')
+const more7 = document.querySelector('.js-more-7')
+const more8 = document.querySelector('.js-more-8')
+const more9 = document.querySelector('.js-more-9')
+const more10 = document.querySelector('.js-more-10')
+const more11 = document.querySelector('.js-more-11')
+const more12 = document.querySelector('.js-more-12')
+
+const info1 = document.querySelector('.js-info-1')
+const info2 = document.querySelector('.js-info-2')
+const info3 = document.querySelector('.js-info-3')
+const info4 = document.querySelector('.js-info-4')
+const info5 = document.querySelector('.js-info-5')
+const info6 = document.querySelector('.js-info-6')
+const info7 = document.querySelector('.js-info-7')
+const info8 = document.querySelector('.js-info-8')
+const info9 = document.querySelector('.js-info-9')
+const info10 = document.querySelector('.js-info-10')
+const info11 = document.querySelector('.js-info-11')
+const info12 = document.querySelector('.js-info-12')
+
+
+
+
+more1.addEventListener('click', () => {
+    info1.classList.toggle('description')
+})
+
+more2.addEventListener('click', () => {
+    info2.classList.toggle('description')
+})
+
+more3.addEventListener('click', () => {
+    info3.classList.toggle('description')
+})
+
+more4.addEventListener('click', () => {
+    info4.classList.toggle('description')
+})
+
+more5.addEventListener('click', () => {
+    info5.classList.toggle('description')
+})
+
+more6.addEventListener('click', () => {
+    info6.classList.toggle('description')
+})
+
+more7.addEventListener('click', () => {
+    info7.classList.toggle('description')
+})
+
+more8.addEventListener('click', () => {
+    info8.classList.toggle('description')
+})
+
+more9.addEventListener('click', () => {
+    info9.classList.toggle('description')
+})
+
+more10.addEventListener('click', () => {
+    info10.classList.toggle('description')
+})
+
+more11.addEventListener('click', () => {
+    info11.classList.toggle('description')
+})
+
+more12.addEventListener('click', () => {
+    info12.classList.toggle('description')
+})
